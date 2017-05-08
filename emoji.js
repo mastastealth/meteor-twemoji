10e2a6f517c3ac38351b4d889b52195d7e5bdd2a
Brian Franco <mastastealth+git@gmail.com>
2017-05-07 22:24:14 -0400

    Swap PNG for SVG

--- emoji.js
+++ emoji.js
@@ -1,27 +1,64 @@
 Emoji = {};
 
-Emoji.baseImagePath = '/packages/davidfrancisco_twemoji/img/';
+Emoji.baseImagePath = '/packages/mastastealth_twemoji/img/';
+
+function dec2hex( str ) { return (str+0).toString(16); }
+
+function emoji2Unicode(str) { 
+  let haut = 0, 
+      n = 0,
+      cp,
+      emojiCode = '';
+
+  for (let i=0; i<str.length; i++) {
+    let b = str.charCodeAt(i); 
+
+    if (b < 0 || b > 0xFFFF) emojiCode += `Error: byte out of range ${dec2hex(b)}!`;
+
+    if (haut != 0) {
+      if (0xDC00 <= b && b <= 0xDFFF) {
+        emojiCode += '-' + dec2hex(0x10000 + ((haut - 0xD800) << 10) + (b - 0xDC00));
+        haut = 0;
+        continue;
+      } else {
+        emojiCode += `Error: surrogate out of range ${dec2hex(haut)}!`;
+        haut = 0;
+      }
+    }
+
+    if (0xD800 <= b && b <= 0xDBFF) {
+      haut = b;
+    } else {
+        cp = dec2hex(b); 
+        emojiCode += `-${cp}`; 
+    }
+  }
+
+  return emojiCode.substring(1);
+}
 
 Emoji.convert = function (str) {
   if (typeof str !== 'string') {
     return '';
+  } else {
+    str = emoji2Unicode(str);
   }
 
   return str.replace(/:[\+\-a-z0-9_]+:/gi, function(match) {
-    var imgName = match.slice(1, -1),
-      path = Emoji.baseImagePath + imgName + '.png';
-    return '<img class="emoji" alt="' + match + '" src="' + path + '" draggable="false" />';
+    let imgName = match.slice(1, -1),
+        path = `${Emoji.baseImagePath}${imgName}.png`;
+    return `<img class="emoji" alt="${match}" src="${path}" draggable="false" />`;
   });
 };
 
 // borrowed code from https://github.com/meteor/meteor/blob/devel/packages/showdown/template-integration.js
 if (Package.templating) {
-  var Template = Package.templating.Template;
-  var Blaze = Package.blaze.Blaze; // implied by `templating`
-  var HTML = Package.htmljs.HTML; // implied by `blaze`
+  let Template = Package.templating.Template,
+      Blaze = Package.blaze.Blaze; // implied by `templating`
+      HTML = Package.htmljs.HTML; // implied by `blaze`
 
   Blaze.Template.registerHelper('emoji', new Template('emoji', function () {
-    var view = this,
+    let view = this,
         content = '';
 
     if (view.templateContentBlock) {
